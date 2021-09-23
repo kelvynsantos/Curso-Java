0674fd9c75c106ec86b180bbbe291fd984e4135c
Kelvyn dos Santos <kelvynsantos10@outlook.com>
2021-09-23 17:50:39 -0300

    reorganização e mudança de IDE

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package fundamentos;
+//import java.util.Random;
+public class PrimeiroPrograma { 
+	public static void main(String[] args) { 
+		 String frase = "ola";
+		 boolean number1 = false;
+		 int number2 = 10;
+		 double percent = 23.33; 
+	    System.out.println(frase); 
+		System.out.println("Meu primeiro programa!!"); 
+		System.out.println(number1); 
+		System.out.println(number2); 
+		System.out.println(percent); 
+		 sun(10,15);
+		 System.out.println();
+	} 
+	static void sun(int x, int y) {
+		System.out.println(x + y);
+		}
+}
---
+++ src/fundamentos/areacircunferencia.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class areacircunferencia {  
+	public static void main(String[] args) {  
+		double raio = 3;
+		 final double pi = 3.141526; 
+		 double area = pi * raio * raio; 
+		System.out.println(area);
+		 System.out.println();
+		  System.out.println();
+	}
+}
