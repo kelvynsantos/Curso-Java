21a1054283cb4a30257437fcafd9207e391f5193
Kelvyn dos Santos <kelvynsantos10@outlook.com>
2023-01-03 13:57:13 -0300

    final treatment-errors e noSQL

--- src/Tratamentos_Erros/Finally.java
+++ src/Tratamentos_Erros/Finally.java
@@ -6,12 +6,12 @@ public class Finally {
      Scanner input = new Scanner(System.in); //tirar static
     public static void main(String[] args) {
         try {
-            System.out.println(7 / input.nextInt());
+//            System.out.println(7 / input.nextInt());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }finally {
             System.out.println("finalmente");
-            input.close();
+//            input.close();
         }
         System.out.println("end");
     }
