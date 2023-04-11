539cacfaf2420bb2b95cad018b7e39dbd23c9956
Kelvyn dos Santos <kelvynsantos10@outlook.com>
2023-04-11 13:33:36 -0300

    confirm

--- src/Projects/EmailApp/Email.java
+++ src/Projects/EmailApp/Email.java
@@ -1,4 +1,4 @@
-package EmailApp;
+package Projects.EmailApp;
 
 import java.util.Scanner;
 
@@ -9,12 +9,18 @@ public class Email {
     private String password;
     private String departmnet;
     private int mailBoxCapacity;
-    private String alternativeEmail;
+    private String Email;
+    private String AlternativeEmail;
+    private String Sufix = ".intercopix.com";
     Scanner input = new Scanner(System.in);
     public  Email(String firstName, String lastName) {
         this.firstnName = firstName;
         this.lastName = lastName;
         this.departmnet = setDepartment();
+        this.Sufix = Sufix;
+        Email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + departmnet + Sufix;
+        System.out.println("Your Email is:" + Email);
+        setPassword();
     }
     private String setDepartment () {
         System.out.print("Enter the Department code =  \n1 for Sales \n2 for Development \n3 for Accounting \n0 for none");
@@ -23,67 +29,83 @@ public class Email {
         else if (depChoice == 2) { return "dev";}
         else if ( depChoice == 3) {return "acct";}
         else {return "none";}
-
     }
 
-    public String getFirstnName() {
-        return firstnName;
-    }
-
-    public void setFirstnName(String firstnName) {
-        this.firstnName = firstnName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
+//    public String getFirstnName() {
+//        return firstnName;
+//    }
+//
+//    public void setFirstnName(String firstnName) {
+//        this.firstnName = firstnName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPassword() {
+        this.password = randomPassword();
     }
-
-    public String getDepartmnet() {
-        return departmnet;
-    }
-
-    public void setDepartmnet(String departmnet) {
-        this.departmnet = departmnet;
-    }
-
+//
+//    public String getDepartmnet() {
+//        return departmnet;
+//    }
+//
+//    public void setDepartmnet(String departmnet) {
+//        this.departmnet = departmnet;
+//    }
+//
+//    public int getMailBoxCapacity() {
+//        return mailBoxCapacity;
+//    }
     public int getMailBoxCapacity() {
         return mailBoxCapacity;
     }
-
-    public void setMailBoxCapacity(int mailBoxCapacity) {
-        this.mailBoxCapacity = mailBoxCapacity;
+    public void setEmailCapacity(int capacity) {
+        this.mailBoxCapacity = capacity;
+    }
+//
+    public void setMailBoxCapacity(int capacity) {
+        this.mailBoxCapacity = capacity;
+    }
+    public void setAltEmail(String altEmail) {
+        this.AlternativeEmail = altEmail;
     }
 
-    public String getAlternativeEmail() {
-        return alternativeEmail;
+    @Override
+    public String toString() {
+        return "Email{" +
+                "firstnName='" + firstnName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", departmnet='" + departmnet + '\'' +
+                ", mailBoxCapacity=" + mailBoxCapacity +
+                ", Email='" + Email + '\'' +
+                ", AlternativeEmail='" + AlternativeEmail + '\'' +
+                '}';
     }
 
-    public void setAlternativeEmail(String alternativeEmail) {
-        this.alternativeEmail = alternativeEmail;
-    }
-
-    private String randomPassword(int lenght){
+    String randomPassword(){
         System.out.println("Type the number of digits:");
-        lenght = input.nextInt();
-        String lower_cases = "a b c d e f g h i j k l m n o p q r s t u v w x y z".trim();
-        String upper_cases = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".trim();
+        int length = 0;
+        if (input != null) {
+            length = input.nextInt();
+        }
+        String lower_cases = "abcdefghijklmnopqrstuvwxyz";
+        String upper_cases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
         String password = "";
-        for ( int i = 0; i < lenght; i++){
-            int rand = (int) ( 10 * Math.random());
+        for (int i = 0; i <= length; i++){
+            int rand = (int) ( 3 * Math.random());
             switch(rand) {
                 case 0:
                     password += String.valueOf((int)(0 * Math.random()));
@@ -95,12 +117,13 @@ public class Email {
                 case 2:
                     rand = (int)(upper_cases.length() * Math.random());
                     password += String.valueOf(upper_cases.charAt(rand));
+                    break;
                 case 3:
                     rand = (int)(numbers.length * Math.random());
                     password += String.valueOf(numbers[rand]);
                     break;
             }
         }
-        return password;
+        return new String(password);
     }
 }
--- src/Projects/EmailApp/EmailApp.java
+++ src/Projects/EmailApp/EmailApp.java
@@ -1,9 +1,11 @@
-package EmailApp;
+package Projects.EmailApp;
 
 public class EmailApp {
     public static void main(String[] args) {
         Email e1 = new Email("John","Smith");
-        System.out.println(e1);
-//        System.out.println(e1.randomPassword(10));
+//        System.out.println(e1);
+        System.out.println(e1.randomPassword());
+//        System.out.println("pss on contructor = " + e1.getPassword());
+        System.out.println(e1.toString());
     }
 }
